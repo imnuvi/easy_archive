e387d375dd5a2eea400fd0c4c080f0922820b113
thedoodler <ramprakashsmail@gmail.com>
2020-10-21 17:56:04 +0530

    rats problem initial

---
+++ rats_and_rats.py
@@ -0,0 +1,12 @@
+#given initial number of rats, find the difference between populations in white and black rats after a particular number of years
+#white rats give birth to two rats in first year, gives birth to three rats in the second year and then dies
+#black rats give birth to two three rats in the first year, then dies in the second year
+
+
+
+years ,rats = list(map(int,input().split()))
+
+
+def black(years,rats):
+
+    
