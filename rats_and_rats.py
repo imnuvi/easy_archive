e39a0d4a75456f6f055330f4acb60d84dd6b382c
thedoodler <ramprakashsmail@gmail.com>
2020-12-08 23:54:08 +0530

    LOGIC: finished logic for white black rat problem

--- rats_and_rats.py
+++ rats_and_rats.py
@@ -9,23 +9,19 @@ years ,rats = list(map(int,input().split()))
 # white = black = rats
 
 def black(years,rats):
-    prev = 0
-    current = rats
-    for i in range(years+1):
-        current = (current*3)
-    return current
+    return int(rats*(3**(years-1)) + rats*(3**(years)))
 
 def white(years,rats):
-    prev = 0
-    two_prev = 0
-    three_prev = 0
-    current = rats
-    for i in range(years-1):
-        current = (current*2) + (prev * 3) + (two_prev) - (three_prev)
-        three_prev = two_prev
-        two_prev = prev
-        prev = current
-    return current
+    x ,y ,z = 7, 2, 1
+    if (years == 0):
+        return rats*1
+    elif (years == 1):
+        return rats*2 + rats*1
+    else:
+        for i in range(years-2):
+            x,y,z = (x*2)+(y*3),x,y
+        return (x+y+z)*rats
 
-print(black(years,rats))
+
+print(white(years,rats))
 # print(abs(white(years,rats)-black(years,rats)))
