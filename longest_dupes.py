48a54b4d5ef6f1383151d0dd1ece74cf4f040924
thedoodler <ramprakashsmail@gmail.com>
2020-12-19 23:55:15 +0530

    FIX:  logic for longest stuff

--- longest_dupes.py
+++ longest_dupes.py
@@ -6,22 +6,31 @@ a = input()
 temp = [a[0]]
 ans = a[0]
 curans = a[0]
+
+longe = ""
 for i in range(1,len(a)):
-    if (a[i] == ans[-1]):
-        curans += a[i]
-    else:
-        curans = a[i]
     if a[i] in temp:
+        if (a[i] == ans[-1]):
+            curans += a[i]
+        else:
+            curans = a[i]
         ans += a[i]
     elif(len(temp) < 2):
         temp.append(a[i])
+        curans = a[i]
         ans += a[i]
     else:
         ans = curans
+        curans = a[i]
         temp.pop(0)
         temp.append(a[i])
+    if (len(ans) >= longe):
+        longe = ans
 
-    print(curans)
+    # print(ans)
+    # print(curans)
+    # print(temp)
+print(longe)
 
 
 
