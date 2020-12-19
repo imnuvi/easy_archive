fe9b814b956747cc8c622141d0702f02e42034b1
thedoodler <ramprakashsmail@gmail.com>
2020-12-19 21:20:07 +0530

    EDIT:  adjustments to logic

--- longest_dupes.py
+++ longest_dupes.py
@@ -3,10 +3,16 @@
 a = input()
 
 
-temp = []
-ans = ""
+temp = [a[0]]
+ans = a[0]
 for i in a:
     if i in temp:
         ans += i
     else:
-        
+        temp.append(i)
+        ans += i
+
+
+
+# sample test case
+# aassddddff
