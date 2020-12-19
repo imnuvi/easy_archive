f7bd61ac7b213db02d0c9561c2ebce286fb16510
thedoodler <ramprakashsmail@gmail.com>
2020-12-19 23:40:17 +0530

    BUG:  curval bug

--- longest_dupes.py
+++ longest_dupes.py
@@ -1,16 +1,27 @@
-
+import re
 
 a = input()
 
 
 temp = [a[0]]
 ans = a[0]
-for i in a:
-    if i in temp:
-        ans += i
+curans = a[0]
+for i in range(1,len(a)):
+    if a[i] in temp:
+        ans += a[i]
+    elif(len(temp) < 2):
+        temp.append(a[i])
+        ans += a[i]
     else:
-        temp.append(i)
-        ans += i
+        ans = curans
+        temp.pop(0)
+        temp.append(a[i])
+    if (a[i] == ans[-1]):
+        curans += a[i]
+    else:
+        curans = a[i]
+    print(curans)
+
 
 
 
