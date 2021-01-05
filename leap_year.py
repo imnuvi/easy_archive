f13159738311707cad7d16641d4dde66d37ac86f
cosmoglint <ramprakashsmail@gmail.com>
2021-01-05 22:19:44 +0530

    ADD:  boolean solution

--- leap_year.py
+++ leap_year.py
@@ -1,4 +1,9 @@
 # given a year print weather it is a leap year or not
 
+
+year = int(input())
 def leap(year):
-    pass
+    return bool(not(year%400) or (not(year%4) and (year%100)))
+
+
+print("Yes" if leap(year) else "Nah")
