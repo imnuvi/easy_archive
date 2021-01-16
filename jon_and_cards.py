ac6ad7add7e76bf23ce5a949dc33a13ecb902bc4
cosmoglint <ramprakashsmail@gmail.com>
2021-01-16 16:58:37 +0530

    ADD:  jon and cards problem

---
+++ jon_and_cards.py
@@ -0,0 +1,4 @@
+# jon has to follow the following steps in order to open his encrypted mail
+# 1. delete all vowels
+# 2. insert a character "*" before each consonant
+# 3. replace all uppercase consonant with lower case
