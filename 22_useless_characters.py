fa4449cabcdfa8badb46b7b69a7daad5845349bc
ram prakash <ramprakashsmail@gmail.com>
2021-01-30 17:22:22 +0530

    ALITER:  for useless characters

--- 22_useless_characters.py
+++ 22_useless_characters.py
@@ -1,5 +1,15 @@
 import re
 
-a = raw_input()
+a = input()
 ans = re.findall("[@#*!]",a)
 print(len(ans))
+
+
+cnt = 0
+for i in a:
+    if i.isalnum():
+        continue
+    else:
+        cnt+=1
+
+print(cnt)
