96bb1096a69f57e5801a827ae3c0a110aa46c98b
ram prakash <ramprakashsmail@gmail.com>
2021-02-04 16:09:39 +0530

    ADD:  second min max problem

---
+++ second_min_max.py
@@ -0,0 +1,11 @@
+a = list(map(int,input().split()))
+
+newl = list(set(a))
+newl.sort()
+
+if len(newl == 1):
+    print(2*newl[0],end="")
+elif len(newl == 2):
+    print(newl[0]+newl[1],end="")
+else:
+    print(newl[1] + newl[-2],end="")
