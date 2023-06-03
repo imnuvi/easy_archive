17f2a75e5b0fccfadcc2b89583a1fcdc567d184f
cosmoglint <ramprakashsmail@gmail.com>
2023-06-03 22:49:58 +0530

    ADD: paranthesis problem

--- 36_matching_paranthesis.exs
+++ 36_matching_paranthesis.exs
@@ -1,3 +1,12 @@
+#Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#
+#An input string is valid if:
+#
+#Open brackets must be closed by the same type of brackets.
+#Open brackets must be closed in the correct order.
+#Every close bracket has a corresponding open bracket of the same type.
+
+
 #defmodule Solution do
   #@spec is_valid(s :: String.t) :: boolean
   #def stack_construct(value, stack) do
@@ -24,12 +33,6 @@
         #|> Enum.map_reduce(acc_stack, &stack_construct/2)
   #end
 #end
-
-
-
-
-
-
 
 defmodule Solution do
   @spec is_valid(s :: String.t) :: boolean
