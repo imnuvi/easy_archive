e3986c6a993feb852fe40c39dc4fc3c430066b49
cosmoglint <ramprakashsmail@gmail.com>
2023-06-22 21:39:01 +0530

    ADD: elixir is substring problem

---
+++ 43_is_subsequence_problem.exs
@@ -0,0 +1,23 @@
+defmodule Solution do
+  @spec is_subsequence(s :: String.t, t :: String.t) :: boolean
+  def sub_fun(main_str, sub_str) do
+    m_first = Enum.at(main_str, 0)
+    s_first = Enum.at(sub_str, 0)
+    cond do
+        Enum.count(sub_str) == 0 ->
+            true
+        Enum.count(main_str) == 0 ->
+            false
+        m_first == s_first ->
+            sub_fun(Enum.slice(main_str, 1..-1) , Enum.slice(sub_str, 1..-1) )
+        true ->
+            sub_fun(Enum.slice(main_str, 1..-1) , sub_str)
+    end
+  end
+
+  def is_subsequence(s, t) do
+    f_s = String.split(s, "", trim: true)
+    f_t = String.split(t, "", trim: true)
+    sub_fun(f_t, f_s)
+  end
+end
