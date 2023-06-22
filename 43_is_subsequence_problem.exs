75330ccd88f72348c0acb11c06e9f2c36ac30a60
cosmoglint <ramprakashsmail@gmail.com>
2023-06-22 21:40:53 +0530

    ADD: substring question changes

--- 43_is_subsequence_problem.exs
+++ 43_is_subsequence_problem.exs
@@ -1,3 +1,9 @@
+# https://leetcode.com/problems/is-subsequence/description/
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
 defmodule Solution do
   @spec is_subsequence(s :: String.t, t :: String.t) :: boolean
   def sub_fun(main_str, sub_str) do
